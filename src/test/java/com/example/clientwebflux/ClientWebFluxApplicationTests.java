1a19049a7b2e70f3d678d270ef68374f984e7b3b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-11-27 12:27:59 +0500

    Initial commit

---
+++ src/test/java/com/example/clientwebflux/ClientWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
